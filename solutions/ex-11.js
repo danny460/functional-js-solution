ccc25e0c701087f303ff1f3c15c174774bb650cb
Dannyzlh <Danielzlh62@gmail.com>
2017-06-22 00:15:41 +0800

    initial commit

---
+++ solutions/ex-11.js
@@ -0,0 +1,6 @@
+/**
+ * Exercise 11. Implement Map with Reduce
+ */
+module.exports = function arrayMap(arr, fn) {
+    return Array.prototype.reduce.call(arr, (prev, curr, index, _arr) => prev.concat([fn(curr, index, _arr)]), []);
+}
---
+++ solutions/ex-12.js
@@ -0,0 +1,12 @@
+/**
+ * Exercise 12. Function Spies
+ */
+module.exports = function Spy(target, method) {
+    const fn = target[method];
+    const obj = { count: 0 };
+    target[method] = function () {
+        obj.count++;
+        fn.apply(this, arguments);
+    };
+    return obj;
+}
